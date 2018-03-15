ce8d6b164b7fabe899827a2cb79647e9874a7e83
Alex Yu <j620656786206@gmail.com>
2018-03-15 15:39:19 -0500

    88. Exceptions and Buffered Reader

--- app/src/main/java/com/alexyu/top10downloader/MainActivity.java
+++ app/src/main/java/com/alexyu/top10downloader/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -52,11 +53,27 @@ public class MainActivity extends AppCompatActivity {
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 int response = connection.getResponseCode();
                 Log.d(TAG, "downloadXML: The response code is " + response);
-                InputStream inputStream = connection.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader reader = new BufferedReader(inputStreamReader);
+//                InputStream inputStream = connection.getInputStream();
+//                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//                BufferedReader reader = new BufferedReader(inputStreamReader);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                int charsRead;
+                char[] inputBuffer = new char[500];
+                while(true) {
+                    charsRead = reader.read(inputBuffer);
+                    if(charsRead < 0) {
+                        break;
+                    }
+                    if(charsRead > 0) {
+                        xmlResult.append(String.valueOf(inputBuffer, 0, charsRead));
+                    }
+                }
+                reader.close();
             } catch(MalformedURLException e) {
                 Log.e(TAG, "downloadXML: Invalid URL " + e.getMessage());
+            } catch (IOException e) {
+                Log.e(TAG, "downloadXML: IO Exception: "+ e.g );
             }
         }
     }

6dadecadce1d44259bd5f15f3ffe16ca01b76c46
Alex Yu <j620656786206@gmail.com>
2018-03-16 12:55:40 -0500

    93. XML Parsing Factory Classes

--- app/src/main/java/com/alexyu/top10downloader/ParseApplications.java
+++ app/src/main/java/com/alexyu/top10downloader/ParseApplications.java
@@ -1,5 +1,9 @@
 package com.alexyu.top10downloader;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
 import java.util.ArrayList;
 
 /**
@@ -16,5 +20,27 @@ public class ParseApplications {
 
     public ArrayList<FeedEntry> getApplications() {
         return applications;
+    }
+
+    public boolean parse(String xmlData) {
+        boolean status = true;
+        FeedEntry currentRecord;
+        boolean inEntry = false;
+        String textValue = "";
+
+        try{
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new StringReader(xmlData));
+            int eventType = xpp.getEventType();
+            while(eventType != XmlPullParser.END_DOCUMENT) {
+
+            }
+
+        } catch(Exception e) {
+            status = false;
+            e.printStackTrace();
+        }
     }
 }

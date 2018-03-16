882beaa447781cb23bb3b233b6846600cc05560e
Alex Yu <j620656786206@gmail.com>
2018-03-16 16:15:13 -0500

    94. Main XML Parsing Loop

--- app/src/main/java/com/alexyu/top10downloader/ParseApplications.java
+++ app/src/main/java/com/alexyu/top10downloader/ParseApplications.java
@@ -1,5 +1,7 @@
 package com.alexyu.top10downloader;
 
+import android.util.Log;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
@@ -24,21 +26,58 @@ public class ParseApplications {
 
     public boolean parse(String xmlData) {
         boolean status = true;
-        FeedEntry currentRecord;
+        FeedEntry currentRecord = null;
         boolean inEntry = false;
         String textValue = "";
 
-        try{
+        try {
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             factory.setNamespaceAware(true);
             XmlPullParser xpp = factory.newPullParser();
             xpp.setInput(new StringReader(xmlData));
             int eventType = xpp.getEventType();
-            while(eventType != XmlPullParser.END_DOCUMENT) {
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String tagName = xpp.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        Log.d(TAG, "parse: Starting tag for " + tagName);
+                        if ("entry".equalsIgnoreCase(tagName)) {
+                            inEntry = true;
+                            currentRecord = new FeedEntry();
+                        }
+                        break;
 
+                    case XmlPullParser.TEXT:
+                        textValue = xpp.getText();
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        Log.d(TAG, "parse: Ending tag for" + tagName);
+                        if (inEntry) {
+                            if ("entry".equalsIgnoreCase(tagName)) {
+                                applications.add(currentRecord);
+                                inEntry = false;
+                            } else if ("name".equalsIgnoreCase(tagName)) {
+                                currentRecord.setName(textValue);
+                            } else if ("artist".equalsIgnoreCase(tagName)) {
+                                currentRecord.setArtist(textValue);
+                            } else if ("releaseDate".equalsIgnoreCase(tagName)) {
+                                currentRecord.setReleaseDate(textValue);
+                            } else if ("summary".equalsIgnoreCase(tagName)) {
+                                currentRecord.setSummary(textValue);
+                            } else if ("image".equalsIgnoreCase(tagName)) {
+                                currentRecord.setImageURL(textValue);
+                            }
+                        }
+                        break;
+
+                    default:
+                        //Nothing to do
+                }
+                eventType = xpp.next();
             }
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             status = false;
             e.printStackTrace();
         }

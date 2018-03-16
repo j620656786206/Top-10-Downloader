65fd20a4fa3385a5b9a0489f3274d61e8e5fcaef
Alex Yu <j620656786206@gmail.com>
2018-03-16 12:43:54 -0500

    92. Processing XML Data

---
+++ app/src/main/java/com/alexyu/top10downloader/ParseApplications.java
@@ -0,0 +1,16 @@
+package com.alexyu.top10downloader;
+
+import java.util.ArrayList;
+
+/**
+ * Created by j on 2018/3/16.
+ */
+
+public class ParseApplications {
+    private static final String TAG = "ParseApplications";
+    private ArrayList<FeedEntry> applications;
+
+    public ParseApplications() {
+        this.applications = new ArrayList<>();
+    }
+}
